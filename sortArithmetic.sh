652c9c737569f9430f3a796887c52e9baadc1bdd
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-01-27 08:57:05 +0530

    ADD.Compute a%b+c

--- sortArithmetic.sh
+++ sortArithmetic.sh
@@ -7,3 +7,5 @@ output2=`awk 'BEGIN{print '$a'*'$b'+'$c'}'`
 echo "a*b+c:"$output2           #$(($a*$b+$c))
 output3=`awk 'BEGIN{print '$c'+'$a'/'$b'}'`
 echo "c+a/b:"$output3           #$(($c+$a/$b))
+output4=`awk 'BEGIN{print '$a'%'$b'+'$c'}'`
+echo "a%b+c:"$output4           #$(($a%$b+$c))
