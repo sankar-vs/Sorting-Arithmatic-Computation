73ad07432042204c11a39fcaf2f3aed3275fcfd7
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-01-27 08:51:57 +0530

    ADD.Compute a*b+c

--- sortArithmetic.sh
+++ sortArithmetic.sh
@@ -3,3 +3,5 @@ read -p "Enter three numbers: " a b c
 echo "a:$a b:$b c:$c"
 output1=`awk 'BEGIN{print '$a'+'$b'*'$c'}'`
 echo "a+b*c:"$output1           #$(($a+$b*$c))
+output2=`awk 'BEGIN{print '$a'*'$b'+'$c'}'`
+echo "a*b+c:"$output2           #$(($a*$b+$c))
