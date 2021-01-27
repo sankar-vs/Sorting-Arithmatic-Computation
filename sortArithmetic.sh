165624da1c92e3b2e422774958df48bf7307a890
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-01-27 09:03:04 +0530

    REFACTOR.Storing the results in a Dictionary for every computation

--- sortArithmetic.sh
+++ sortArithmetic.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
 read -p "Enter three numbers: " a b c
+declare -A myDict
 echo "a:$a b:$b c:$c"
-output1=`awk 'BEGIN{print '$a'+'$b'*'$c'}'`
-echo "a+b*c:"$output1           #$(($a+$b*$c))
-output2=`awk 'BEGIN{print '$a'*'$b'+'$c'}'`
-echo "a*b+c:"$output2           #$(($a*$b+$c))
-output3=`awk 'BEGIN{print '$c'+'$a'/'$b'}'`
-echo "c+a/b:"$output3           #$(($c+$a/$b))
-output4=`awk 'BEGIN{print '$a'%'$b'+'$c'}'`
-echo "a%b+c:"$output4           #$(($a%$b+$c))
+myDict[case1]=`awk 'BEGIN{print '$a'+'$b'*'$c'}'`
+echo "a+b*c:"${myDict[case1]}           #$(($a+$b*$c))
+myDict[case2]=`awk 'BEGIN{print '$a'*'$b'+'$c'}'`
+echo "a*b+c:"${myDict[case2]}           #$(($a*$b+$c))
+myDict[case3]=`awk 'BEGIN{print '$c'+'$a'/'$b'}'`
+echo "c+a/b:"${myDict[case3]}           #$(($c+$a/$b))
+myDict[case4]=`awk 'BEGIN{print '$a'%'$b'+'$c'}'`
+echo "a%b+c:"${myDict[case4]}           #$(($a%$b+$c))
